2a6d21b264a03bacb9bb5fc194a87c2ec3d66140
Hugo Vallemont <hugo.vallemont@viacesi.fr>
2024-04-08 15:43:35 +0200

    -removed '/' at the end of routes

--- src/routes/gitRoutes.js
+++ src/routes/gitRoutes.js
@@ -3,8 +3,8 @@ const { getRepos, getBranches, getFiles, getFile } = require('../views/gitView')
 const gitRoutes = function(instance, opts, next) {
 
     instance.get('/', getRepos);
-    instance.get('/:repo/', getBranches);
-    instance.get('/:repo/:branch/', getFiles);
+    instance.get('/:repo', getBranches);
+    instance.get('/:repo/:branch', getFiles);
     instance.get('/:repo/:branch/*', getFile);
     next();
 };
