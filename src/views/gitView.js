6b13b4e591db70e1a9961e97a154b4c7295eaeb3
LavauxEsteban <166394065+LavauxEsteban@users.noreply.github.com>
2024-04-09 13:36:20 +0200

    remis le contenu du fichier

--- src/views/gitView.js
+++ src/views/gitView.js
@@ -1,1 +1,13 @@
-this is a test
+const {  } = require('../utils/swagger.schema');
+const { getRepos, getBranches, getFiles, getFile, download } = require('../views/gitView');
+const gitRoutes = function(instance, opts, next) {
+
+    instance.get('/', getRepos);
+    instance.get('/:repo', getBranches);
+    instance.get('/:repo/:branch', getFiles);
+    instance.get('/:repo/:branch/*', getFile);
+    //instance.get('/download/:repo/:branch/*', download);
+    next();
+};
+
+module.exports = { gitRoutes };
