29038ff3166e38de13e7ef7f072644f17e627838
LavauxEsteban <166394065+LavauxEsteban@users.noreply.github.com>
2024-04-10 15:50:49 +0200

    testing

--- src/routes/gitRoutes.js
+++ src/routes/gitRoutes.js
@@ -1,13 +1,1 @@
-const {  } = require('../utils/swagger.schema');
-const { getRepos, getBranches, getFiles, getFile, download } = require('../views/gitView');
-const gitRoutes = function(instance, opts, next) {
-
-    instance.get('/', getRepos);
-    instance.get('/:repo', getBranches);
-    instance.get('/:repo/:branch', getFiles);
-    instance.get('/:repo/:branch/*', getFile);
-    //instance.get('/download/:repo/:branch/*', download);
-    next();
-};
-
-module.exports = { gitRoutes };
+this is a test
