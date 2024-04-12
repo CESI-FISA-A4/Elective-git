64ee47ec278c8e3a8bf91cb9c91b793be0fc6a89
LavauxEsteban <166394065+LavauxEsteban@users.noreply.github.com>
2024-04-12 08:51:34 +0100

    fix git routes

--- src/routes/gitRoutes.js
+++ src/routes/gitRoutes.js
@@ -1,1 +1,15 @@
-this is a test test test testtesttttt
+const {  } = require('../utils/swagger.schema');
+const { getRepos, getBranches, getFiles, getFile, download, commit, ping } = require('../views/gitView');
+const gitRoutes = function(instance, opts, next) {
+
+    instance.get('/ping', ping);
+    instance.get('/', getRepos);
+    instance.get('/:repo', getBranches);
+    instance.get('/:repo/:branch', getFiles);
+    instance.get('/:repo/:branch/*', getFile);
+    instance.put('/:repo/:branch/*', commit);
+    instance.get('/download/:repo/:branch/*', download);
+    next();
+};
+
+module.exports = { gitRoutes };
